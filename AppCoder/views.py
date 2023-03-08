3fa217e63b0fe79d489562ab6598a4af5b759bff
Marcelo Ruiz <serviplay82@gmail.com>
2023-03-08 19:59:46 -0300

    Clase de herencias

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,10 +5,21 @@ from AppCoder.models import Curso
 
 # Create your views here.
 def cursos(request):
-    return render(request, "index.html")
+    all_cursos = Curso.objects.all()
+    context = {
+        "cursos": all_cursos
+    }
+    return render(request, "AppCoder/cursos.html")
 
+def crear_curso(request, nombre, camada):
+    save_curso = Curso(nombre=nombre, camada=int(camada))
+    save_curso.save()
+    context = {
+        "nombre": nombre
+    }
+    return render(request, "AppCoder/save_curso.html, context")
 def estudiantes(request):
-    return render(request, "index.html")
+    return render(request, "base.html",)
 
 def profesores(request):
-    return render(request, "index.html")
+    return render(request, "base.html")
