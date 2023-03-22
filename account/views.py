88a4c21639153a949125641f864f2b0e58018030
Marcelo Ruiz <serviplay82@gmail.com>
2023-03-22 17:18:20 -0300

    Tercera pre-entrega Ruiz

--- account/views.py
+++ account/views.py
@@ -1,28 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout, authenticate
-from
+
+from account.forms import UserRegisterForm
+
 
 # Create your views here.
+
 def register_account(request):
     if request.method == "POST":
         # form = UserCreationForm(request.POST)
-        form = UserRegisterForm(request.form)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
            form.save()
            return redirect("accountLogin")
 
-    form = UserCreationForm()
+    form = UserRegisterForm()
     context = {
-        form : form
+        "form": form
     }
-    return render(request, "account/login.html",context=context)
+    return render(request, "account/login.html", context=context)
 
 
 def login_account(request):
 
     if request.method == "POST":
-        form = AuthenticationForm(request.POST)
+        form = AuthenticationForm(request, data=request.POST)
 
         if form.is_valid():
             informacion = form.cleaned_data
@@ -31,12 +35,12 @@ def login_account(request):
             if user is not None:
                 login(request, user)
 
-                return redirect("AppCoderCursos")
+                return render(request, "account/login.html", context={"mensajes":["Logueado con exito"]})
             else:
-                return redirect("AppCoderEstudiantes")
-
+                return render(request, "account/login.html", context={"mensajes":["Ingrese los datos correctos"]})
+                # return redirect("AppCoderCursos") OTRA FORMA ES REDIRECCIONARLO A OTRA PAGINA.
     form = AuthenticationForm()
     context = {
-        "form" : form
+        "form": form
     }
     return render(request, "account/login.html", context=context)
