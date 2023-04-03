8ffaa53f5c89ffc023b8ede870776db1d7f38381
Marcelo Ruiz <serviplay82@gmail.com>
2023-04-03 17:53:46 -0300

    2do commit

--- turnero/admin.py
+++ turnero/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+# get all the models from the models.py file
+# and register them here
+admin.site.register(Atencion)
+admin.site.register(Cliente)
+admin.site.register(Persona)
+admin.site.register(Servicio)
+
